27bca809c8c83fa2356b2fbaa886fe8f408fe4da
GuillaumeTaffin <guillaume.taffinjob@gmail.com>
2023-05-08 14:16:07 -0400

    Add logo icon

---
+++ src/main/kotlin/com/github/guillaumetaffin/intellij/ligo/language/common/LigoIcons.kt
@@ -0,0 +1,9 @@
+package com.github.guillaumetaffin.intellij.ligo.language.common
+
+import com.intellij.openapi.util.IconLoader
+
+object LigoIcons {
+
+    val LOGO = IconLoader.getIcon("/icons/logo.svg", LigoIcons::class.java)
+
+}
