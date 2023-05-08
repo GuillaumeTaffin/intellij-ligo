5658cbd2cd10c5159dc9bb75a8c90864cb917ed7
GuillaumeTaffin <guillaume.taffinjob@gmail.com>
2023-05-08 14:11:45 -0400

    Create JsLigoLanguage class

---
+++ src/main/kotlin/com/github/guillaumetaffin/intellij/ligo/language/jsligo/JsLigoLanguage.kt
@@ -0,0 +1,12 @@
+package com.github.guillaumetaffin.intellij.ligo.language.jsligo
+
+import com.intellij.lang.Language
+
+class JsLigoLanguage: Language("JsLigo") {
+
+    companion object {
+        @JvmStatic
+        val INSTANCE = JsLigoLanguage()
+    }
+
+}
