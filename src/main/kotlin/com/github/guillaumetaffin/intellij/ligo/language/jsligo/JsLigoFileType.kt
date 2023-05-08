d94fc17245c49c357ff15efe228b1a98d3a40073
GuillaumeTaffin <guillaume.taffinjob@gmail.com>
2023-05-08 14:28:04 -0400

    Setup JsLigoFileType support

---
+++ src/main/kotlin/com/github/guillaumetaffin/intellij/ligo/language/jsligo/JsLigoFileType.kt
@@ -0,0 +1,21 @@
+package com.github.guillaumetaffin.intellij.ligo.language.jsligo
+
+import com.github.guillaumetaffin.intellij.ligo.language.common.LigoIcons
+import com.intellij.openapi.fileTypes.LanguageFileType
+import javax.swing.Icon
+
+class JsLigoFileType: LanguageFileType(JsLigoLanguage.INSTANCE) {
+
+    companion object {
+        @JvmStatic
+        val INSTANCE = JsLigoFileType()
+    }
+
+    override fun getName(): String = "JsLigo File"
+
+    override fun getDescription(): String = "JsLigo language file"
+
+    override fun getDefaultExtension(): String = "jsligo"
+
+    override fun getIcon(): Icon = LigoIcons.LOGO
+}
