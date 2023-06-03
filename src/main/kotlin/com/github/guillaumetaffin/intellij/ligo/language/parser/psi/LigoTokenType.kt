c5a7eaa500802997005dc7b67a07d39d644a06d6
GuillaumeTaffin <guillaume.taffinjob@gmail.com>
2023-06-03 19:08:16 -0400

    Simplify the grammar using the textmate lexing available

--- src/main/kotlin/com/github/guillaumetaffin/intellij/ligo/language/parser/psi/LigoTokenType.kt
+++
@@ -1,7 +0,0 @@
-package com.github.guillaumetaffin.intellij.ligo.language.parser.psi
-
-import com.github.guillaumetaffin.intellij.ligo.language.LigoLanguage
-import com.intellij.psi.tree.IElementType
-
-class LigoTokenType(debugName: String) : IElementType(debugName, LigoLanguage.INSTANCE) {
-}
